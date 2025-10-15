f0a8788596a22d4b347ec8227da68237335139df
saifuldesignpcl-netizen <saifuldesign.pcl@gmail.com>
2025-10-15 16:43:58 +0600

    Update wsgi.py

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -6,7 +6,7 @@ project_home = '/home/yourusername/myapp'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myapp.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
